2d26385811d65c274dbe6d02cf3b627d9d10d50d
Jamie <grayarea@runbox.com>
2019-10-18 14:22:22 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Moving and changing file permissions and making executable 
+sudo mv /home/pi/piplug.service /etc/systemd/system
+sudo chmod 644 /etc/systemd/system/piplug.service
+chmod +x /home/pi/piplug.py
